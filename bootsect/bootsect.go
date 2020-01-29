788eea76958b18f924754d55229b9fe139057d0f
t9t <t9t@users.noreply.github.com>
2020-01-29 12:57:17 +0100

    Parse bytes/clusters

--- bootsect/bootsect.go
+++ bootsect/bootsect.go
@@ -3,19 +3,24 @@ package bootsect
 import "encoding/binary"
 
 type BootSector struct {
-	OemId                              string
-	BytesPerSector                     int
-	SectorsPerCluster                  int
-	MediaDescriptor                    byte
-	SectorsPerTrack                    int
-	NumberofHeads                      int
-	HiddenSectors                      int
-	TotalSectors                       uint64
-	MftClusterNumber                   uint64
-	MftMirrorClusterNumber             uint64
-	BytesOrClusterPerFileSecordSegment byte
-	BytersOrClusterPerIndexBuffer      byte
-	VolumeSerialNumber                 []byte
+	OemId                  string
+	BytesPerSector         int
+	SectorsPerCluster      int
+	MediaDescriptor        byte
+	SectorsPerTrack        int
+	NumberofHeads          int
+	HiddenSectors          int
+	TotalSectors           uint64
+	MftClusterNumber       uint64
+	MftMirrorClusterNumber uint64
+	FileRecordSegmentSize  BytesOrClusters
+	IndexBufferSize        BytesOrClusters
+	VolumeSerialNumber     []byte
+}
+
+type BytesOrClusters struct {
+	IsBytes bool
+	Value   int
 }
 
 var endian = binary.LittleEndian
@@ -23,20 +28,29 @@ var endian = binary.LittleEndian
 func Parse(data []byte) BootSector {
 	r := BinReader(data)
 	return BootSector{
-		OemId:                              string(r.Read(0x03, 8)),
-		BytesPerSector:                     int(r.Uint16(0x0B)),
-		SectorsPerCluster:                  int(r[0x0D]),
-		MediaDescriptor:                    r[0x15],
-		SectorsPerTrack:                    int(r.Uint16(0x18)),
-		NumberofHeads:                      int(r.Uint16(0x1A)),
-		HiddenSectors:                      int(r.Uint16(0x1C)),
-		TotalSectors:                       r.Uint64(0x28),
-		MftClusterNumber:                   r.Uint64(0x30),
-		MftMirrorClusterNumber:             r.Uint64(0x38),
-		BytesOrClusterPerFileSecordSegment: r[0x40],
-		BytersOrClusterPerIndexBuffer:      r[0x44],
-		VolumeSerialNumber:                 dupe(r.Read(0x48, 8)),
+		OemId:                  string(r.Read(0x03, 8)),
+		BytesPerSector:         int(r.Uint16(0x0B)),
+		SectorsPerCluster:      int(r[0x0D]),
+		MediaDescriptor:        r[0x15],
+		SectorsPerTrack:        int(r.Uint16(0x18)),
+		NumberofHeads:          int(r.Uint16(0x1A)),
+		HiddenSectors:          int(r.Uint16(0x1C)),
+		TotalSectors:           r.Uint64(0x28),
+		MftClusterNumber:       r.Uint64(0x30),
+		MftMirrorClusterNumber: r.Uint64(0x38),
+		FileRecordSegmentSize:  parseBytesOrClusters(r[0x40]),
+		IndexBufferSize:        parseBytesOrClusters(r[0x44]),
+		VolumeSerialNumber:     dupe(r.Read(0x48, 8)),
 	}
+}
+
+func parseBytesOrClusters(b byte) BytesOrClusters {
+	i := int(int8(b))
+	if i < 0 {
+		value := 1 << -i
+		return BytesOrClusters{IsBytes: true, Value: value}
+	}
+	return BytesOrClusters{IsBytes: false, Value: int(b)}
 }
 
 func dupe(in []byte) []byte {
--- bootsect/bootsect_test.go
+++ bootsect/bootsect_test.go
@@ -5,7 +5,6 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-
 	"github.com/t9t/gomft/bootsect"
 )
 
@@ -17,19 +16,19 @@ func TestParse(t *testing.T) {
 
 	ret := bootsect.Parse(b)
 	expected := bootsect.BootSector{
-		OemId:                              "NTFS    ",
-		BytesPerSector:                     512,
-		SectorsPerCluster:                  8,
-		MediaDescriptor:                    0xF8,
-		SectorsPerTrack:                    63,
-		NumberofHeads:                      255,
-		HiddenSectors:                      10240,
-		TotalSectors:                       0x745b8210,
-		MftClusterNumber:                   0xc0000,
-		MftMirrorClusterNumber:             0x2,
-		BytesOrClusterPerFileSecordSegment: 0xF6,
-		BytersOrClusterPerIndexBuffer:      0x1,
-		VolumeSerialNumber:                 []byte{0xA3, 0x70, 0xD7, 0x4C, 0x31, 0x11, 0x5C, 0x3E},
+		OemId:                  "NTFS    ",
+		BytesPerSector:         512,
+		SectorsPerCluster:      8,
+		MediaDescriptor:        0xF8,
+		SectorsPerTrack:        63,
+		NumberofHeads:          255,
+		HiddenSectors:          10240,
+		TotalSectors:           0x745b8210,
+		MftClusterNumber:       0xc0000,
+		MftMirrorClusterNumber: 0x2,
+		FileRecordSegmentSize:  bootsect.BytesOrClusters{IsBytes: true, Value: 1024},
+		IndexBufferSize:        bootsect.BytesOrClusters{IsBytes: false, Value: 1},
+		VolumeSerialNumber:     []byte{0xA3, 0x70, 0xD7, 0x4C, 0x31, 0x11, 0x5C, 0x3E},
 	}
 
 	assert.Equal(t, expected, ret)
