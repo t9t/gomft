6a169b102efbb82ec2d9655803f14cb40f5f485e
t9t <t9t@users.noreply.github.com>
2020-02-01 16:05:06 +0100

    UTF16 string decoder

---
+++ utf16/decode.go
@@ -0,0 +1,21 @@
+package utf16
+
+import (
+	"errors"
+	"unicode/utf16"
+	"encoding/binary"
+)
+
+func DecodeString(b []byte, bo binary.ByteOrder) (string, error) {
+	blen := len(b)
+	if blen %2 != 0 {
+		return "", errors.New("input data must have even number of bytes")
+	}
+	slen := blen/2
+	shorts := make([]uint16, slen)
+	for i := 0; i < slen; i++ {
+		bi := i * 2
+		shorts[i] = bo.Uint16(b[bi:bi+2])
+	}
+	return string(utf16.Decode(shorts)), nil
+}
